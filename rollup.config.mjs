14893057b90e17d02b9608b29da30271eab2aa54
zsmoore <zsmoore@zachary-moore.com>
2024-10-19 20:00:24 -0400

    Initial bootstrap

---
+++ rollup.config.mjs
@@ -0,0 +1,36 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import del from "rollup-plugin-delete";
+import packageJson from "./package.json" with { type: "json" };
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.module,
+        format: "cjs",
+        sourcemap: true,
+      },
+    ],
+    external: ['react-dom'],
+    plugins: [
+      resolve(),
+      commonjs(),
+      typescript({ 
+        tsconfig: "./tsconfig.json",
+       }),
+    ],
+  },
+  {
+    input: "dist/types/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts(),
+      del({ hook: "buildEnd", targets: "./dist/types" })
+    ],
+  },
+];
+
+
