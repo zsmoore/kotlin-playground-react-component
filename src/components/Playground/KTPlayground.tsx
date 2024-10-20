af057cb3ea43f912f16100ac442a3c209ab2c9b2
zsmoore <zsmoore@zachary-moore.com>
2024-10-19 20:44:03 -0400

    Simple impl

---
+++ src/components/Playground/KTPlayground.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import playground from 'kotlin-playground';
+
+export interface KTPlaygroundProps {
+  code: string;
+}
+
+const KTPLayground = (props: KTPlaygroundProps) => {
+  useEffect(() => {
+    playground('code');
+  });
+  return <div>
+    <code>
+      {props.code}
+    </code>
+  </div>
+}
+
+export default KTPLayground
