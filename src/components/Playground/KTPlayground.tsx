f11ed1a79baeb138a5cbde024a094606ba06fab7
zsmoore <zsmoore@zachary-moore.com>
2024-10-21 00:51:41 -0400

    Defer import and restructure rollup to support ssr

--- src/components/Playground/KTPlayground.tsx
+++ src/components/Playground/KTPlayground.tsx
@@ -1,14 +1,22 @@
-import React, { useEffect, PropsWithChildren, createElement } from 'react';
+import React, {
+  useEffect,
+  PropsWithChildren,
+  createElement,
+  useState,
+} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { KTPlaygroundProps } from './Props';
-import playground from 'kotlin-playground';
 import { convertPropsToAttrs, getPlaygroundOptions } from './util/utils';
 
 const KTPlayground = (props: PropsWithChildren<KTPlaygroundProps>) => {
-  const id = 'id' + uuidv4().toString();
+  const [id, setId] = useState('dummy');
   useEffect(() => {
-    playground('.' + id, getPlaygroundOptions(props));
-  });
+    setId('id' + uuidv4().toString());
+    // Defer import for SSR purposes
+    import('kotlin-playground').then((module) => {
+      module.default('.' + id, getPlaygroundOptions(props));
+    });
+  }, []);
 
   let children = props.children;
   if (props.code) {
